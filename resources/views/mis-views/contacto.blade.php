b14c2ba30ae0fe4457bab63ca60cd7a572251a94
Josue Martinez <jmartinezolivares1@gmail.com>
2022-09-02 09:25:22 -0600

    Fix contacto view style

--- resources/views/mis-views/contacto.blade.php
+++ resources/views/mis-views/contacto.blade.php
@@ -10,17 +10,21 @@
             </div>
         @endif
 
-        <form method="POST">
-            @csrf
-            <label for="nombre">Nombre:</label>
-            <input type="text" id="nombre" name="nombre" value="{{old('nombre')}}" class="form-control" autocomplete="off"><br/>
-            <label for="email">Email:</label>
-            <input type="text" id="email" name="email" value="{{old('email')}}" class="form-control" autocomplete="off"><br/>
-            <input type="checkbox" id="publicidad" name="publicidad" @checked(old('publicidad', true))>
-            <label for="publicidad">Recibir Publicidad</label><br>
-            <label for="mensaje">Mensaje:</label> <br/>
-            <textarea id="mensaje" name="mensaje" class="form-control"> {{old('mensaje')}}</textarea><br/>
-            <input type="submit" value="Enviar" class="form-control btn btn-outline-primary"/>
-        </form>
+        <div class="main-container">
+            <div class="container-fluid">
+                <form method="POST">
+                    @csrf
+                    <label for="nombre">Nombre:</label>
+                    <input type="text" id="nombre" name="nombre" value="{{old('nombre')}}" class="form-control" autocomplete="off"><br/>
+                    <label for="email">Email:</label>
+                    <input type="text" id="email" name="email" value="{{old('email')}}" class="form-control" autocomplete="off"><br/>
+                    <input type="checkbox" id="publicidad" name="publicidad" @checked(old('publicidad', true))>
+                    <label for="publicidad">Recibir Publicidad</label><br>
+                    <label for="mensaje">Mensaje:</label> <br/>
+                    <textarea id="mensaje" name="mensaje" class="form-control"> {{old('mensaje')}}</textarea><br/>
+                    <input type="submit" value="Enviar" class="form-control btn btn-outline-primary"/>
+                </form>
+            </div>
+        </div>
     @endsection
 </x-layout>
